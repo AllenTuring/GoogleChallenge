86e2d796d59d7d34ef93ef47f9098d9b85944e67
Allen Jiang <allen.jiang@outlook.com>
2017-08-09 14:02:07 -0400

    Added framework of challenge 1, braille translation 2

---
+++ braille_translation_2/solution.java
@@ -0,0 +1,9 @@
+package​ ​com.google.challenges;​ ​
+
+public​ ​class​ ​Answer​ ​{​ ​​ ​​ ​
+​ ​​ ​​ ​​ ​public​ ​static​ ​String​ ​answer(String​ ​plaintext)​ ​{​ ​
+
+​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​//​ ​Your​ ​code​ ​goes​ ​here.
+
+​ ​​ ​​ ​​ ​}​ ​
+}
---
+++ braille_translation_2/solution.py
@@ -0,0 +1,2 @@
+def​ ​answer(plaintext):
+​ ​​ ​​ ​​ ​#​ ​your​ ​code​ ​here
