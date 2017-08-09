8ac85adb8df2ec6fa83e215e3e5a63a233d3d52b
Allen Jiang <allen.jiang@outlook.com>
2017-08-09 18:52:19 -0400

    finalized version of minimum minions count, pre-notarization

--- lovely_lucky_lambs/solution.py
+++ lovely_lucky_lambs/solution.py
@@ -23,8 +23,8 @@ def find_minumum_henchmen(total_lambs):
 		n += 1
 
 	# Are there enough left to pay another without breaking the fib rule?
-	last_1 = 2**(n-1) if n > 0 else 0
-	last_2 = 2**(n-2) if n > 1 else 0
+	last_1 = 2**(n-1) if n > 0 else 0 # how much the last goon was paid
+	last_2 = 2**(n-2) if n > 1 else 0 # how much 2nd-to-last goon was paid
 	if total_lambs >= last_1 + last_2:
 		n += 1
 
@@ -51,5 +51,16 @@ def test_max():
 
 def test_min():
 	assert find_minumum_henchmen(1) == 1
-	assert find_minumum_henchmen(2) == 2
 	assert find_minumum_henchmen(10) == 3
+	# simple ranges
+	for i in range(2, 6):
+		assert find_minumum_henchmen(i) == 2
+	for i in range(6, 13):
+		assert find_minumum_henchmen(i) == 3
+	# derived range test
+	# caution! scales as 2^n
+	n_min, n_max = 0, 1
+	for n in range(1, 13):
+		n_min, n_max = n_max, (2**n - 1) + math.floor(2**(n-1)) + math.floor(2**(n-2))
+		for i in range(n_min, n_max):
+			assert find_minumum_henchmen(i) == n
